84ea90a1e32b353fa152a4b1ebfefcc35136f4bb
aakanksha-bhandari <aakanksha.bhandari@gmail.com>
2019-06-27 18:00:15 +0100

    Start adding the data and types

--- app.js
+++ app.js
@@ -4,18 +4,35 @@ const graphqlHttp = require('express-graphql');
 const {buildSchema} = require('graphql');
 
 const app = express();
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use(
     '/graphql',
     graphqlHttp({
         schema: buildSchema(`
+
+            type Event {
+                _id: ID!
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+            input EventInput{
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
             type RootQuery {
-                events: [String!]!
+                events: [Event!]!
             }
 
             type RootMutation {
-                createEvent(name: String): String
+                createEvent(eventInput: EventInput): Event
             }
 
             schema {
@@ -25,12 +42,18 @@ app.use(
         `),
         rootValue: {
             events: () => {
-                return ['Event 1', 'Event 2', 'Event 3'];
+                return events;
             },
             createEvent: (args) => {
-                const eventName = args.name;
-                return eventName;
-               
+                const event = {
+                    _id: Math.random().toString(),
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price,
+                    date: args.eventInput.date
+                };
+                events.push(event);
+                return event;               
             } 
         },
         graphiql: true
