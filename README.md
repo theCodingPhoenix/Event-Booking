7785ba27f6cd815272e8dd68db412543d060014c
aakanksha-bhandari <aakanksha.bhandari@gmail.com>
2019-06-27 19:56:17 +0100

    Add the mongoose connection to MongoDB

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ MERN stack project utilising GraphQL APIs
 ``` npm init 
 npm install --save express body-parser
 npm install --save express-graphql graphql
+npm install --save mongoose
 ```
--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const {buildSchema} = require('graphql');
+const mongoose = require('mongoose');
+const Event = require('./models/events');
 
 const app = express();
 const events = [];
@@ -42,18 +44,41 @@ app.use(
         `),
         rootValue: {
             events: () => {
-                return events;
+                // get all the events from the db
+                // use the return here to let express know that this is an async operation
+                return Event.find()
+                .then(events => {
+                    return events.map(event => {
+                        return {...event._doc};
+                    })
+                })
+                .catch(err => {
+                    console.log(err);
+                });
             },
             createEvent: (args) => {
-                const event = {
-                    _id: Math.random().toString(),
+
+                // build the js object
+                const event = new Event({
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: args.eventInput.date
-                };
-                events.push(event);
-                return event;               
+                    date: new Date(args.eventInput.date)
+               });
+
+               // return the event so the Graphql knows about the asyn operation
+               return event
+                .save()
+                .then(result => {
+                    // result is basically the event that we get
+                    // however there is meta data attached to it
+                    // so use the _doc method to get the json data
+                    return {...result._doc};
+                })
+                .catch(err => {
+                    console.log(err);
+                    throw err;
+                })
             } 
         },
         graphiql: true
@@ -61,4 +86,10 @@ app.use(
 );
 
 
-app.listen(3000);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${
+    process.env.MONGO_PASSWORD
+}@cluster0-vfwck.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`).then(() => {
+    app.listen(3000);
+}).catch(err => {
+    console.log(err);
+});
